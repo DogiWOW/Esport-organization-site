bfc306ced90138aa87fab6222d0829c55f884574
Mateusz <twonightdragons@gmail.com>
2024-09-21 13:27:43 +0200

    Updated contents of ContactUs.tsx

--- src/components/Sections/ContactUs/ContactUs.tsx
+++ src/components/Sections/ContactUs/ContactUs.tsx
@@ -5,10 +5,10 @@ const ContactUs = () => {
     <div className='section'>
         <div className='w-screen justify-center items-center'>
             <div className='text-center break-words overflow-hidden sm:text-base md:text-2xl'>
-                <p className='pt-5'>Email</p>
-                <p className='py-2'>loremipsumdolor@lorem.com</p>
+                <p className='pt-5'>Email:</p>
+                <p className='py-2'>lucid.dreamers@dreamers.com</p>
                 <p className='pt-5'>Facebook: </p>
-                <p className='py-2'><a href='https://www.facebook.com/groups/1634961666903617' target='_blank' rel='noreferrer'>https://www.facebook.com/groups/1634961666903617</a></p>
+                <p className='py-2'>'https://www.facebook.com/groups/163496166</p>
             </div>
         </div>
     </div>
